5e0dfadc86fc81d1f9be68de099cf2a6beb569a1
Viktoria-Zharova <zharova_cofee@mail.ru>
2024-05-17 23:33:18 +0200

    added components and some fixes in docker-compose file

---
+++ frontend/vite-number-conversion/src/pages/TaskPage/TaskPage.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const TaskPage: React.FC = () => {
+
+    return (
+        <div className="container">
+            <Header />
+            <Task />
+        </div>
+    );
+};
+
+export default TaskPage;
---
+++ frontend/vite-number-conversion/src/pages/singleModule/SingleModule.tsx
@@ -0,0 +1,13 @@
+// import React, {useState} from "react";
+import Header from "../../components/header/Header.tsx";
+
+const SingleModule: React.FC = () => {
+    return (
+        <div className="container">
+            <Header />
+        </div>
+    );
+};
+
+export default SingleModule;
+

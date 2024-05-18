aeafab8005d190648aefe00e82e32952bb3113a8
Viktoria-Zharova <zharova_cofee@mail.ru>
2024-05-18 10:52:12 +0200

    some fixes

---
+++ frontend/vite-number-conversion/src/components/Module/Module.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import './Module.css';
+
+const Module: React.FC = () => {
+    return (
+        <div className="container">
+
+        </div>
+    );
+};
+
+export default Module;
